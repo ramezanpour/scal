e4c3700fad1fd868d8333ceed0bcb04c64baaac8
Mohammad Mahdi Ramezanpour <i@ramezanpour.net>
2020-01-27 15:58:05 +0330

    Add licence and --version flag

--- main.c
+++ main.c
@@ -1,9 +1,33 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2020 Mohammad Mahdi Ramezanpour
+
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include "convertor.h"
+
+#define VERSION "0.0.1"
 
 void draw_cal(int d, int m, int y)
 {
@@ -83,6 +107,11 @@ int main(int argc, char const *argv[])
     }
     else
     {
+        if (has_flag("--version", argv, argc))
+        {
+            printf("SCAL Version %s\n", VERSION);
+            return 0;
+        }
         if (has_flag("--simple", argv, argc))
         {
             // Return a simple date
